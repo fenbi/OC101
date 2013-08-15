cc8ecd351eb6fb4a8ea74b4032df86c81f0e5a7a
liluo <i@liluo.org>
2013-08-15 20:59:47 +0800

    Initial Commit

---
+++ Calc/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Calc
+//
+//  Created by liluo on 8/15/13.
+//  Copyright (c) 2013 liluo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Calc/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Calc
+//
+//  Created by liluo on 8/15/13.
+//  Copyright (c) 2013 liluo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
